4722d4c59b8fa02002e7ab8cc66dceff6228b48d
DarienAllison1 <124842514+DarienAllison1@users.noreply.github.com>
2024-12-02 16:17:55 -0500

    pdate main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example usage of Utils methods
+        int[][] magicSquare = {
+            {9, 3, 7},
+            {4, 2, 8},
+            {4, 9, 2}
+        };
+        System.out.println("Is magic square: " + Utils.isMagicSquare(magicSquare));
+
+        int[] array = {1, 2, 3, 4, 2};
+        System.out.println("Count of unique subarrays: " + Utils.countUniqueSubarrays(array, 5));
+
+        String str = "abacdfgdcaba";
+        System.out.println("Longest palindromic substring: " + Utils.longestPalindromicSubstring(str));
+    }
+}
